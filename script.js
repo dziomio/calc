36e3e77f0f48091d7b4ef28f271b9f7b43a1ed67
Yevheniy Kaduk <sartoriamodo@gmail.com>
2019-11-05 22:40:00 +0100

    calcoliatore

---
+++ script.js
@@ -0,0 +1,27 @@
+let strt = document.getElementById('start');
+console.log(strt);
+
+let butIncome = document.getElementsByTagName('button.income_add');
+console.log(butIncome);
+
+let butExpenses = document.getElementsByTagName('button.expenses_add');
+
+let depositCheck = document.querySelector('#deposit-check');
+
+let addIncomeItem = document.querySelectorAll('.additional_income-item');
+
+let budgetDayValue = document.getElementsByClassName('.budget_day-value');
+let expensesMonthValue = document.getElementsByClassName('.expenses_month-value');
+let additionalIncomeValue = document.getElementsByClassName('.additional_income-value');
+let additionalExpensesValue = document.getElementsByClassName('.additional_expenses-value');
+let incomePeriodValue = document.getElementsByClassName('.income_period-value');
+let targetMonthValue = document.getElementsByClassName('.target_month-value');
+
+let salaryAmount = document.querySelector('.salary-amount');
+let incomeTitle = document.querySelector('.income-title');
+let incomeAmount = document.querySelector('.income-amount');
+let additionalIncomeItem = document.querySelector('.additional_income-item');
+let expensesTitle = document.querySelector('.expenses-title');
+let expensesAmount = document.querySelector('.expenses-amount');
+let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+let periodSelect = document.querySelector('.period-select');
